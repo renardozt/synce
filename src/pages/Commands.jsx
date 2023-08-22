75057e70f5831c4df109400d2b790e5b69a5695a
Renardev <renardev@outlook.com>
2023-08-21 18:19:15 -0700

    Some command changes

--- src/pages/Commands.jsx
+++ src/pages/Commands.jsx
@@ -53,6 +53,16 @@ export default class Commands extends Component {
             newArr = array;
         }
 
+        newArr = newArr?.sort(function (a, b) {
+            if (a.name < b.name) {
+              return -1;
+            }
+            if (a.name > b.name) {
+              return 1;
+            }
+            return 0;
+          });
+
         return newArr;
     }
 
